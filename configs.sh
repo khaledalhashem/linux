2aa327dc6c54f498c1a181d68066629d11e1f9fb
Khaled AlHashem <kmalhashem@gmail.com>
2018-11-11 08:33:11 +0300

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -87,8 +87,7 @@ tee firewall.sh <<EOF
 #
 #SSH
  iptables -A INPUT -p tcp --dport 22 -j ACCEPT
- iptables -A INPUT -p tcp --dport 28 -j ACCEPT
-#HTTP
+ #HTTP
  iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 #HTTPS
  iptables -A INPUT -p tcp --dport 443 -j ACCEPT
